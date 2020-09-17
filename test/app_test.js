c3f7fb710342c5f4e8f6ca0d0fa8925465821584
Werberson Silva <werberson.silva@gmail.com>
2020-09-17 13:06:00 -0300

    change addr label value from the original URL to the registered route path

--- test/app_test.js
+++ test/app_test.js
@@ -21,10 +21,30 @@ app.post('/test', (req, res) => {
 	res.send('test post')
 })
 
+app.get('/testPathParameter/:parameter', (req, res) => {
+	res.status(200)
+	res.send('test post')
+})
+app.post('/testPathParameter/:param1/:param2', (req, res) => {
+	res.status(400)
+	res.set("Error-Message", `Test Error Message: param1=${req.params.param1} , param2=${req.params.param2}`)
+	res.send()
+})
+
 const router = express.Router()
 
 router.get('/testRouter', (req, res) => {
 	res.send("test router")
+})
+
+router.get('/testPath/:parameter', (req, res) => {
+	res.send("parameter " + req.params.parameter);
+})
+
+router.post('/testPath/:parameter1/action/:parameter2', (req, res) => {
+	res.status(400)
+	res.set("Error-Message", `Test Error Message: param1=${req.params.parameter1} , param2=${req.params.parameter2}`)
+	res.send("test 2 path parameters")
 })
 
 app.use('/router', router)
